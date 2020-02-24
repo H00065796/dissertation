5ae5e248f6d36ea177f1a9181eb9515d39b4c5dd
Ahmed Dirie <contact@ahmeddirie.com>
2020-02-23 20:30:12 -0500

    Setting up codebuild projects

---
+++ projects/singular/codebuild_web.tf
@@ -0,0 +1,33 @@
+##############################################
+# CodeBuild - Web
+##############################################
+
+module "web_codebuild_project" {
+  source          = "git::https://github.com/H00065796/dissertation//modules/codebuild"
+  name            = "${var.env_name}-${var.web_name}"
+  description     = "Code build project for ${var.env_name}-${var.web_name}"
+  environment     = var.env_name
+  build_timeout   = "5"
+  service_role    = module.codebuild_iam_role.arn
+  artifacts_type  = "NO_ARTIFACTS"
+  cache_type      = "NO_CACHE"
+  compute_type    = "BUILD_GENERAL1_MEDIUM"
+  compute_image   = "aws/codebuild/docker:18.09.0"
+  compute_os      = "LINUX_CONTAINER"
+  privileged_mode = true
+  s3_type         = "S3"
+  s3_location     = "${var.env_name}-build/${var.web_name}/"
+  env_tag         = var.env_tag
+  s3_buildspec    = ""
+
+  environment_variables = [
+    {
+      name  = "TARGET_ENV"
+      value = "env"
+    },
+    {
+      name  = "RELEASE_VER"
+      value = "1.00.00"
+    }
+  ]
+}
