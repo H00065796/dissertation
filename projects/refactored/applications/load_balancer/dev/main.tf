f37ba2d33840117f218499886c7f4e212526be12
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-02 17:29:15 -0500

    Creating main and outputs

---
+++ projects/refactored/applications/load_balancer/dev/main.tf
@@ -0,0 +1,33 @@
+##############################################
+# AWS
+##############################################
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.0"
+}
+
+##############################################
+# Terraform and Backend
+##############################################
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "liverpool-tf-state"
+    key    = "project02/applications/load_balancer/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
+
+##############################################
+# Referenced Projects
+##############################################
+data "terraform_remote_state" "dev" {
+  backend = "s3"
+
+  config = {
+    bucket = "liverpool-tf-state"
+    key    = "project02/environment/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
