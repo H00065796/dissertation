11f7d48b59bda522d52b4e004f2ef72901936cad
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-02 17:34:39 -0500

    Creating the main and outputs files

---
+++ projects/refactored/applications/rds/dev/main.tf
@@ -0,0 +1,43 @@
+##############################################
+# AWS
+##############################################
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.0"
+}
+
+##############################################
+# Terraform and Backend
+##############################################
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "liverpool-tf-state"
+    key    = "project02/applications/rds/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
+
+##############################################
+# Referenced Projects
+##############################################
+data "terraform_remote_state" "dev" {
+  backend = "s3"
+
+  config = {
+    bucket = "liverpool-tf-state"
+    key    = "project02/environment/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
+
+data "terraform_remote_state" "secrets" {
+  backend = "s3"
+
+  config = {
+    bucket = "liverpool-tf-state"
+    key    = "project02/applications/secrets/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
