aba6f037ed05c6a9aa18971063aeda954f47c1d8
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-02 17:47:10 -0500

    Setting up the main and outputs file for the Dev project

---
+++ projects/refactored/environment/dev/main.tf
@@ -0,0 +1,20 @@
+##############################################
+# AWS
+##############################################
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.0"
+}
+
+##############################################
+# Terraform and Backend
+##############################################
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "liverpool-tf-state"
+    key    = "project02/environment/dev/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
