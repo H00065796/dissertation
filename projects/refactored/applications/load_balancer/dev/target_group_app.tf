3553079259ebc2e3ddcf17396398fb792934d0c8
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-02 17:28:42 -0500

    Creating target groups

---
+++ projects/refactored/applications/load_balancer/dev/target_group_app.tf
@@ -0,0 +1,31 @@
+##############################################
+# Target Groups - App
+##############################################
+
+module "app_target_group_blue" {
+  source                = "git::https://github.com/H00065796/dissertation//modules/load_balancer_target_group"
+  vpc_id                = data.terraform_remote_state.dev.outputs.vpc_id
+  target_group_prefix   = var.app_target_group1_prefix
+  protocol              = var.app_target_group_protocol
+  health_check_protocol = var.app_health_check_protocol
+  health_check_path     = var.app_health_check_path
+  port                  = var.app_target_group1_port
+  target_type           = "instance"
+  health_check_enabled  = true
+  env_tag               = data.terraform_remote_state.dev.outputs.env_tag
+  load_balancer_arn     = module.app_load_balancer.arn
+}
+
+module "app_target_group_green" {
+  source                = "git::https://github.com/H00065796/dissertation//modules/load_balancer_target_group"
+  vpc_id                = data.terraform_remote_state.dev.outputs.vpc_id
+  target_group_prefix   = var.app_target_group2_prefix
+  protocol              = var.app_target_group_protocol
+  health_check_protocol = var.app_health_check_protocol
+  health_check_path     = var.app_health_check_path
+  port                  = var.app_target_group2_port
+  target_type           = "instance"
+  health_check_enabled  = true
+  env_tag               = data.terraform_remote_state.dev.outputs.env_tag
+  load_balancer_arn     = module.app_load_balancer.arn
+}
---
+++ projects/refactored/applications/load_balancer/dev/target_group_web.tf
@@ -0,0 +1,31 @@
+##############################################
+# Target Groups - Web
+##############################################
+
+module "web_target_group_blue" {
+  source                = "git::https://github.com/H00065796/dissertation//modules/load_balancer_target_group"
+  vpc_id                = data.terraform_remote_state.dev.outputs.vpc_id
+  target_group_prefix   = var.web_target_group1_prefix
+  protocol              = var.web_target_group_protocol
+  health_check_protocol = var.web_health_check_protocol
+  health_check_path     = var.web_health_check_path
+  port                  = var.web_target_group1_port
+  target_type           = "instance"
+  health_check_enabled  = true
+  env_tag               = data.terraform_remote_state.dev.outputs.env_tag
+  load_balancer_arn     = module.web_load_balancer.arn
+}
+
+module "web_target_group_green" {
+  source                = "git::https://github.com/H00065796/dissertation//modules/load_balancer_target_group"
+  vpc_id                = data.terraform_remote_state.dev.outputs.vpc_id
+  target_group_prefix   = var.web_target_group2_prefix
+  protocol              = var.web_target_group_protocol
+  health_check_protocol = var.web_health_check_protocol
+  health_check_path     = var.web_health_check_path
+  port                  = var.web_target_group2_port
+  target_type           = "instance"
+  health_check_enabled  = true
+  env_tag               = data.terraform_remote_state.dev.outputs.env_tag
+  load_balancer_arn     = module.web_load_balancer.arn
+}
