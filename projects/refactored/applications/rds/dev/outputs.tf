4865fd94ca38005237cd64a30da2a04dc9877e17
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-04 11:23:34 -0500

    Creating outputs for the RDS Cluster

--- projects/refactored/applications/rds/dev/outputs.tf
+++ projects/refactored/applications/rds/dev/outputs.tf
@@ -1,3 +1,8 @@
-##############################################
-# Outputs
-##############################################
+# MySQL RDS Cluster
+
+output "mysql_arn" { value = module.mysql_rds_cluster.arn }
+output "mysql_id" { value = module.mysql_rds_cluster.id }
+output "mysql_cluster_identifier" { value = module.mysql_rds_cluster.cluster_identifier }
+output "mysql_engine" { value = module.mysql_rds_cluster.engine }
+output "mysql_engine_version" { value = module.mysql_rds_cluster.engine_version }
+output "mysql_cluster_name" { value = var.cluster_name }
