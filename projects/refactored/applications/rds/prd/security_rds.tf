aa37d72092d2cf6e4f81951321f2d1749a0606f7
Ahmed Dirie <contact@ahmeddirie.com>
2020-03-04 11:26:16 -0500

    Creating a PRD project for RDS

---
+++ projects/refactored/applications/rds/prd/security_rds.tf
@@ -0,0 +1,85 @@
+##############################################
+# Security Group - RDS
+##############################################
+
+module "rds_security_group" {
+  source         = "git::https://github.com/H00065796/dissertation//modules/security_group"
+  name           = "rds-security-group"
+  description    = "RDS Security Group"
+  vpc_id         = data.terraform_remote_state.env.outputs.vpc_id
+  env_tag        = data.terraform_remote_state.env.outputs.env_tag
+}
+
+##############################################
+# Ingress Rules
+##############################################
+
+module "rds_security_rules_ingress_app1" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet1}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic from App 1"
+}
+
+module "rds_security_rules_ingress_app2" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet2}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic from App 2"
+}
+
+module "rds_security_rules_ingress_app3" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet3}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic from App 3"
+}
+
+##############################################
+# Egress Rules
+##############################################
+
+module "rds_security_rules_egress_app1" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet1}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic to App 1"
+}
+
+module "rds_security_rules_egress_app2" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet2}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic to App 2"
+}
+
+module "rds_security_rules_egress_app3" {
+  source                   = "git::https://github.com/H00065796/dissertation//modules/security_rules"
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  cidr_blocks              = "${data.terraform_remote_state.env.outputs.app_subnet3}${data.terraform_remote_state.env.outputs.app_subnet_mask}"
+  security_group_id        = module.rds_security_group.id
+  description              = "RDS Traffic to App 3"
+}
